75bae238f3cd1bf43de6c0fff5cc8cd1fac3d508
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-06-02 16:35:11 -0700

    cleaned up the playground

--- Cartwheel/MyPlayground.playground/Contents.swift
+++ Cartwheel/MyPlayground.playground/Contents.swift
@@ -2,32 +2,3 @@
 
 import Cocoa
 
-// MARK: Defaults.plist file
-
-struct CWDefaultsPlist {
-    var cartfileDirectorySearchRecursion: Int
-    var cartListSaveLocation: String
-    
-    init() {
-        var dataError: NSError?
-        var plistError: NSError?
-        let plistURL = NSBundle.mainBundle().URLForResource("CartwheelDefaults", withExtension: "plist")!
-        let data = NSData(contentsOfURL: plistURL, options: nil, error: &dataError)!
-        let plist = NSPropertyListSerialization.propertyListWithData(data, options: NSPropertyListReadOptions.allZeros, format: nil, error: &plistError) as! NSDictionary
-        
-        let cartfileDirectorySearchRecursion = plist["CartfileDirectorySearchRecursion"] as! NSNumber
-        let cartListSaveLocation = plist["CartListSaveLocation"] as! String
-        
-        self.cartfileDirectorySearchRecursion = cartfileDirectorySearchRecursion.integerValue
-        self.cartListSaveLocation = cartListSaveLocation
-    }
-}
-
-extension CWDefaultsPlist: Printable {
-    var description: String {
-        return "CWDefaultsPlist: cartfileDirectorySearchRecursion: \(self.cartfileDirectorySearchRecursion) – cartListSaveLocation: \(self.cartListSaveLocation)"
-    }
-}
-
-let defaults = CWDefaultsPlist()
-println(defaults)
